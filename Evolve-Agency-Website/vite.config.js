c6533153fafa04f0661d25394c9b8f81dfe83cf6
evolveagencymarketing <alejandro@evolvea.net>
2025-09-02 18:46:27 -0600

    Update vite.config.js

--- Evolve-Agency-Website/vite.config.js
+++ Evolve-Agency-Website/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // keep '/' since you'll use your own domain
+  // IMPORTANT: must match your repo name for project Pages
+  base: '/Evolve-Agency-Website-V1/',
   build: { outDir: 'dist' }
 })
